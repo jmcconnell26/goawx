1178802beb7799cca3247e264be8f2a7aac1c250
Josh McConnell <josh@sanity.io>
2023-05-09 10:49:04 +0100

    `gofmt`

--- client/execution_environments.go
+++ client/execution_environments.go
@@ -1,33 +1,33 @@
 package awx
 
 type ExecutionEnvironmentsService struct {
-    client *Client
+	client *Client
 }
 
 type ExecutionEnvironmentsResponse struct {
-    Pagination
-    Results []*ExecutionEnvironment `json:"results"`
+	Pagination
+	Results []*ExecutionEnvironment `json:"results"`
 }
 
 const executionEnvironmentsAPIEndpoint = "/api/v2/execution_environments/"
 
 func (executionEnvironmentsService *ExecutionEnvironmentsService) ListExecutionEnvironments(params map[string]string) ([]*ExecutionEnvironment,
-    *ExecutionEnvironmentsResponse,
-    error) {
-    result := new(ExecutionEnvironmentsResponse)
-    response, err := executionEnvironmentsService.client.Requester.GetJSON(
-        executionEnvironmentsAPIEndpoint,
-        result,
-        params)
+	*ExecutionEnvironmentsResponse,
+	error) {
+	result := new(ExecutionEnvironmentsResponse)
+	response, err := executionEnvironmentsService.client.Requester.GetJSON(
+		executionEnvironmentsAPIEndpoint,
+		result,
+		params)
 
-    if err != nil {
-        return nil, result, err
-    }
+	if err != nil {
+		return nil, result, err
+	}
 
-    err = CheckResponse(response)
-    if err != nil {
-        return nil, result, err
-    }
+	err = CheckResponse(response)
+	if err != nil {
+		return nil, result, err
+	}
 
-    return result.Results, result, nil
+	return result.Results, result, nil
 }
